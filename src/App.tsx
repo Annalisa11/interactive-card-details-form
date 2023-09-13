643342c45124e27a87c0b163335468aa297e799f
Annalisa11 <annalisa.comin@hotmail.com>
2023-09-13 11:52:31 +0200

    added sass + try with grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
-import React from "react";
-import "./App.css";
+import "./App.scss";
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='app'>
+      <div className='container'>
+        <div className='banner'></div>
+        <div className='cards'>
+          <div className='card-front'></div>
+          <div className='card-back'></div>
+        </div>
+        <div className='card-form'>
+          <form className='form'>
+            <div className='inputfield'>
+              <label htmlFor='name'>Label</label>
+              <input type='text' name='name' />
+            </div>
+            <button type='submit' className='button'>
+              Confirm
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
